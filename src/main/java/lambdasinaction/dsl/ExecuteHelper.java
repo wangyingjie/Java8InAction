3f0f906db525aa07f1029684f0ccd28c5983cc0b
wangyingjie1 <wangying>
2017-02-24 21:46:17 +0800

    Java8 新特性测试

--- src/main/java/lambdasinaction/dsl/ExecuteHelper.java
+++ src/main/java/lambdasinaction/dsl/ExecuteHelper.java
@@ -10,18 +10,49 @@ public class ExecuteHelper {
         execute.doExecute();
     }
 
+    /**
+     * 带有返回值得执行方法
+     * @param execute
+     * @param umpKey
+     * @param <T>
+     * @return
+     */
+    public static <T> T execute(Execute execute, String umpKey) {
+
+        System.out.println("------------1111--------"+ Execute.class.getSimpleName());
+        System.out.println("start========" + umpKey);
+        try {
+            return (T)execute.doExecute();
+        } catch (Exception e) {
+            System.out.println("umpKey===>" + umpKey);
+        }
+        System.out.println("end========" + umpKey);
+
+        return null;
+    }
+
     private interface Execute {
-        void doExecute();
+        Object doExecute();
     }
 
     //测试匿名内部类的使用方式
     public static void main(String[] args) {
         ExecuteHelper.execute(() -> Study.print());
+        String xxx = ExecuteHelper.execute(() -> Study.print(), "xxx");
+        String execute = ExecuteHelper.execute(() -> Study.sayHello("zhangsan"), "zzz");
+
+        System.out.println(xxx + "==========" + execute);
     }
 
     public static class Study {
-        public static void print() {
+        public static String print() {
             System.out.println("Hello world*******");
+            return "中国";
+        }
+
+        public static String sayHello(String userName) {
+            System.out.println("say Hello *******" + userName);
+            return "龙的传人";
         }
     }
 }
